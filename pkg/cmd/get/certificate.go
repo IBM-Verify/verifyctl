fe621cb96d0b26638b134aa7b5d6150bc4905b8c
Manjusha Sonawane <manjusha.sonawane@ibm.com>
2025-05-08 11:20:52 +0530

    Refactored get/certificate.go to return required response

--- pkg/cmd/get/certificate.go
+++ pkg/cmd/get/certificate.go
@@ -22,27 +22,23 @@ const (
 
 var (
 	personalCertLongDesc = templates.LongDesc(cmdutil.TranslateLongDesc(personalCertMessagePrefix, `
-Get Verify personal certificate based on an optional filter or a specific personal certificate.
-
-Resources managed on Verify have specific entitlements, so ensure that the application or API client used
-with the 'auth' command is configured with the appropriate entitlements.
-
-You can identify the entitlement required by running:
-
-verifyctl get personalCert --entitlements`))
+		Get Verify personal certificate based on an optional filter or a specific personal certificate.
+ 
+		Resources managed on Verify have specific entitlements, so ensure that the application or API client used with the 'auth' command is configured with the appropriate entitlements.
+ 
+		You can identify the entitlement required by running: verifyctl get personalCert --entitlements`))
 
 	personalCertExamples = templates.Examples(cmdutil.TranslateExamples(personalCertMessagePrefix, `
-# Get a specific personal certificate by lable
-verifyctl get personalCert -o=yaml --name=testpersonalCert
-
-# Get 10 policies based on a given search criteria and sort it in the ascending order by name.
+		# Get a specific personal certificate by lable
+		verifyctl get personalCert -o=yaml --name=testpersonalCert
+ 
+		# Get 10 policies based on a given search criteria and sort it in the ascending order by name.
 		verifyctl get personal-certificate --count=2 --sort=label -o=yaml
-`))
+		`))
 )
 
 type personalCertOptions struct {
 	options
-
 	config *config.CLIConfig
 	label  string
 }
@@ -51,10 +47,9 @@ func newPersonalCertCommand(config *config.CLIConfig, streams io.ReadWriter) *co
 	o := &personalCertOptions{
 		config: config,
 	}
-
 	cmd := &cobra.Command{
 		Use:                   personalCertUsage,
-		Short:                 cmdutil.TranslateShortDesc(personalCertMessagePrefix, "Get Verify password policies based on an optional filter or a specific policy."),
+		Short:                 cmdutil.TranslateShortDesc(personalCertMessagePrefix, "Get Verify personal certificates based on an optional filter or a specific certificate."),
 		Long:                  personalCertLongDesc,
 		Example:               personalCertExamples,
 		Aliases:               []string{"personalCert"},
@@ -65,13 +60,10 @@ func newPersonalCertCommand(config *config.CLIConfig, streams io.ReadWriter) *co
 			cmdutil.ExitOnError(cmd, o.Run(cmd, args))
 		},
 	}
-
 	cmd.SetOut(streams)
 	cmd.SetErr(streams)
 	cmd.SetIn(streams)
-
 	o.AddFlags(cmd)
-
 	return cmd
 }
 
@@ -102,27 +94,45 @@ func (o *personalCertOptions) Run(cmd *cobra.Command, args []string) error {
 		cmdutil.WriteString(cmd, entitlementsMessage+" "+personalCertEntitlements)
 		return nil
 	}
+
 	_, err := o.config.SetAuthToContext(cmd.Context())
 	if err != nil {
 		return err
 	}
+
 	if cmd.CalledAs() == "personalCert" || len(o.label) > 0 {
 		return o.handleSinglePersonalCert(cmd, args)
 	}
 	return o.handlePersonalCertList(cmd, args)
 }
 
-func (o *personalCertOptions) handleSinglePersonalCert(cmd *cobra.Command, _ []string) error {
+func filterPersonalCertData(pcrt *security.PersonalCert) map[string]interface{} {
+	return map[string]interface{}{
+		"notbefore":           pcrt.NotBefore,
+		"subject":             pcrt.Subject,
+		"notafter":            pcrt.NotAfter,
+		"serial_number":       pcrt.SerialNumber,
+		"label":               pcrt.Label,
+		"version":             pcrt.Version,
+		"issuer":              pcrt.Issuer,
+		"isDefault":           pcrt.IsDefault,
+		"keysize":             pcrt.KeySize,
+		"signature_algorithm": pcrt.SignatureAlgorithm,
+	}
+}
 
+func (o *personalCertOptions) handleSinglePersonalCert(cmd *cobra.Command, _ []string) error {
 	c := security.NewPersonalCertClient()
 	pcrt, uri, err := c.GetPersonalCert(cmd.Context(), o.label)
 	if err != nil {
 		return err
 	}
+
 	if o.output == "raw" {
 		cmdutil.WriteAsJSON(cmd, pcrt, cmd.OutOrStdout())
 		return nil
 	}
+
 	resourceObj := &resource.ResourceObject{
 		Kind:       resource.ResourceTypePrefix + "PersonalCert",
 		APIVersion: "1.0",
@@ -130,8 +140,9 @@ func (o *personalCertOptions) handleSinglePersonalCert(cmd *cobra.Command, _ []s
 			Name: pcrt.Label,
 			URI:  uri,
 		},
-		Data: pcrt,
+		Data: filterPersonalCertData(pcrt),
 	}
+
 	if o.output == "json" {
 		cmdutil.WriteAsJSON(cmd, resourceObj, cmd.OutOrStdout())
 	} else {
@@ -146,6 +157,7 @@ func (o *personalCertOptions) handlePersonalCertList(cmd *cobra.Command, _ []str
 	if err != nil {
 		return err
 	}
+
 	if o.output == "raw" {
 		cmdutil.WriteAsJSON(cmd, pcrts, cmd.OutOrStdout())
 		return nil
@@ -159,9 +171,10 @@ func (o *personalCertOptions) handlePersonalCertList(cmd *cobra.Command, _ []str
 			Metadata: &resource.ResourceObjectMetadata{
 				Name: pcrt.Label,
 			},
-			Data: pcrt,
+			Data: filterPersonalCertData(&pcrt),
 		})
 	}
+
 	resourceObj := &resource.ResourceObjectList{
 		Kind:       resource.ResourceTypePrefix + "List",
 		APIVersion: "1.0",
@@ -170,6 +183,7 @@ func (o *personalCertOptions) handlePersonalCertList(cmd *cobra.Command, _ []str
 		},
 		Items: items,
 	}
+
 	if o.output == "raw" || o.output == "json" {
 		cmdutil.WriteAsJSON(cmd, resourceObj, cmd.OutOrStdout())
 	} else {
