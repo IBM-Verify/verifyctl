68c9b2c410ea560420b56ea85fd9d3dcbee29000
Sabuj Mondal <sabuj.mondal@partner.ibm.com>
2025-05-26 17:01:13 +0000

    access policy boilerplate generation

--- pkg/cmd/create/accesspolicy.go
+++ pkg/cmd/create/accesspolicy.go
@@ -111,7 +111,7 @@ func (o *accessPolicyOptions) Run(cmd *cobra.Command, args []string) error {
 		resourceObj := &resource.ResourceObject{
 			Kind:       resource.ResourceTypePrefix + "AccessPolicy",
 			APIVersion: "5.0",
-			Data:       (&security.Policy{}).Example(),
+			Data:       security.AccessPolicyExample(),
 		}
 
 		cmdutil.WriteAsYAML(cmd, resourceObj, cmd.OutOrStdout())
